c79bae1b8e46dc48e6f68bb361353cca5f309cb7
musakanneh <mkanneh17@alustudent.com>
2021-01-06 22:22:54 +0200

    Updated folders structs

--- Hackerrank/7-Strings/common_child.py
+++ Hackerrank/7-Strings/common_child.py
@@ -6,14 +6,14 @@ def common_child(s1, s2):
 
 	"""
 	m, n = len(s1), len(s2)
-	prev, cur = [0]*(n+1), [0]*(n+1)
-	for i in range(1, m+1):
-		for j in range(1, n+1):
-			if s1[i-1] == s2[j-1]:
-				cur[j] = 1 + prev[j-1]
+	prev, cur = [0]*(n + 1), [0]*(n + 1)
+	for i in range(1, m + 1):
+		for j in range(1, n + 1):
+			if s1[i - 1] == s2[j - 1]:
+				cur[j] = 1 + prev[j - 1]
 			else:
-				if cur[j-1] > prev[j]:
-					cur[j] = cur[j-1]
+				if cur[j - 1] > prev[j]:
+					cur[j] = cur[j - 1]
 				else:
 					cur[j] = prev[j]
 					cur, prev = prev, cur
