599aa31cac857b1be35077f6b4f839f0a2b4f058
musakanneh <mkanneh17@alustudent.com>
2021-02-25 12:23:02 +0200

    Updated concepts folder: sliding window

--- 6-leetcode-top-75/1-Medium/0-character-replacement.py
+++ 6-leetcode-top-75/1-Medium/0-character-replacement.py
@@ -1,9 +1,12 @@
-class Solution:
+#!/usr/bin/python3
+
+
+class Solution(object):
     """Longest Repeating Character Replacement
     Given a string s that consists of only uppercase English letters,
     you can perform at most k operations on that string.
     https://leetcode.com/problems/longest-repeating-character-replacement/
-    
+
     """
 
     def character_replacement(self, s, k):
@@ -11,8 +14,18 @@ class Solution:
         traverse the main string of letters
         and store update input string in the temp string.
         then return the length of the temp string
-        
+
         """
         temp_string = ""
-        for i in range(len(s))
-        
+        count = 0
+        for i in range(len(s)):
+            while k:
+                if s[i] == s[i + 1]:
+                    temp_string.append(s[i])
+                count += 1
+        return len(temp_string)
+
+
+characters = "ABAB"
+k = 2
+print(Solution().character_replacement(characters, k))
--- 6-leetcode-top-75/1-Medium/10-word-dictionary.py
+++ 6-leetcode-top-75/1-Medium/10-word-dictionary.py
@@ -8,14 +8,13 @@ class WordDictionary:
     void addWord(word) Adds word to the data structure, it can be matched later.
     bool search(word) Returns true if there is any string in the data structure that
     matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
-    
+
     https://leetcode.com/problems/design-add-and-search-words-data-structure/
-    
+
     """
 
     def __init__(self):
-        """
-        Initialize your data structure here.
+        """Initialize your data structure here.
         """
 
     def addWord(self, word: str):
